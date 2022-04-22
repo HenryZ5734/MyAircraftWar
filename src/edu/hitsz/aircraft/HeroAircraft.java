8fd089d3444df9a813c582a401e63a43f5f57631
HenryZ5734 <200110614@stu.hit.edu.cn>
2022-04-22 16:08:47 +0800

    增加了策略模式和数据访问对象模式的uml类图

--- src/edu/hitsz/aircraft/HeroAircraft.java
+++ src/edu/hitsz/aircraft/HeroAircraft.java
@@ -78,18 +78,6 @@ public class HeroAircraft extends AbstractAircraft {
                 getLocationY(),
                 0,
                 getSpeedY()
-                );
-//        List<BaseBullet> res = new LinkedList<>();
-//        int x = this.getLocationX();
-//        int y = this.getLocationY() + direction*2;
-//        int speedX = 0;
-//        int speedY = this.getSpeedY() + direction*5;
-//        BaseBullet baseBullet;
-//        for(int i=0; i<shootNum; i++){
-//            // 子弹发射位置相对飞机位置向前偏移
-//            // 多个子弹横向分散
-//            baseBullet = new HeroBullet(x + (i*2 - shootNum + 1)*10, y, speedX, speedY, power);
-//            res.add(baseBullet);
-//        }
+        );
     }
 }
